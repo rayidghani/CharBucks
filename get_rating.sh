d80791ba535b5b816ed088a29163999ab45fe65b
Rayid Ghani <rayidghani@gmail.com>
2017-03-15 22:13:23 -0500

    fixed to work with flask

--- get_rating.sh
+++ get_rating.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [ $# -ne 2 ]; then
+if [ $# -ne 1 ]; then
     echo $0: usage: myscript name
     exit 1
 fi
 
 bizname=$1
-imgdir=$2
+imgdir='latteart/images_to_label/'
 
 python get_yelp_images.py $bizname $imgdir
 python label_dir.py $imgdir
