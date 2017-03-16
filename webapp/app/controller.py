c6ade76a9b4de37931feeb3134ffff19e46bc8c3
Rayid Ghani <rayidghani@gmail.com>
2017-03-16 10:33:41 -0500

    reorganized directory structure

---
+++ webapp/app/controller.py
@@ -0,0 +1,27 @@
+from flask import render_template, request, url_for, jsonify
+from app import app, model
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template("index.html")
+
+@app.route('/url', methods=['POST', 'GET'])
+def classify_image_url():
+    if request.method == 'POST':
+        posttext = request.form['posttext']
+        if posttext is not "":
+            result = model.get_score(posttext)
+            print(result)
+            return render_template("url.html", results=result)
+    return render_template("url.html")
+
+@app.route('/bizid', methods=['POST', 'GET'])
+def classify_bizid():
+    if request.method == 'POST':
+        posttext = request.form['posttext']
+        if posttext is not "":
+            result = model.get_biz_score(posttext)
+            print(result)
+            return render_template("bizid.html", results=result)
+    return render_template("bizid.html")
+
---
+++ webapp/app/model.py
@@ -0,0 +1,15 @@
+import subprocess
+
+def get_score(image_url):
+    classify_cmd = "'/latteart/label_web_image.sh " + str(image_url) + "'"
+    cmd = "docker exec 31eaef381caa sh -c " + classify_cmd 
+    p = subprocess.Popen(cmd, shell=True, stdout = subprocess.PIPE)
+    out,err = p.communicate()
+    return out
+
+def get_biz_score(bizid):
+    classify_cmd = "'/latteart/get_rating.sh " + str(bizid) + "'"
+    cmd = "docker exec 31eaef381caa sh -c " + classify_cmd
+    p = subprocess.Popen(cmd, shell=True, stdout = subprocess.PIPE)
+    out,err = p.communicate()
+    return out
